78dba7ec9c7ed69d43f5d03f89a251853e48aa7e
Cielo <houng1004@gmail.com>
2024-08-06 00:11:24 +0800

    feat:react環境設置、安裝storybook和tailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded-md'>Click me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
